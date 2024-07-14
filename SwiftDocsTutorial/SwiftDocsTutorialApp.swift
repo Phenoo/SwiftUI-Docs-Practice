58fc89481bfe0ec48c1138977019032bef72f1c6
Phenoo <descometusah@gmail.com>
2024-07-14 18:31:54 +0100

    Initial Commit

---
+++ SwiftDocsTutorial/SwiftDocsTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftDocsTutorialApp.swift
+//  SwiftDocsTutorial
+//
+//  Created by Eze Chidera Paschal on 14/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftDocsTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
